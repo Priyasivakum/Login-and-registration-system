15d30dbfb7b241c933164d9a6da3c5136f15fb04
Priyadharshini <87526837+Priyasivakum@users.noreply.github.com>
2022-03-08 15:22:44 +0530

    Update login.py

--- login.py
+++ login.py
@@ -6,7 +6,7 @@ def login():
     for line in db:
         x = line.split(",")
         d.append(x[0])
-
+# checks the file in read mode->if the userid exists and password matches,->"logged in"
     if X in d:
         Y=input("Please Enter your password: ")
         Y=Y.strip()
@@ -17,16 +17,16 @@ def login():
             if X==info[0] and Y==info[1]:
                 print("Welcome, logged in successfully!")
             else:
-                F = input("Forgot Password [Y/N] : ")
+                F = input("Forgot Password [Yes/No] : ")
 
-                if F == "N":
+                if F == "No":
                     print("try")
                     login()
 
-                if F == "Y":
+                if F == "Yes":
                     b = input("Create your new password with atleast one capital letter one integer and one special character: ")
                     s = False
-
+#if user forgets password, the same logic is implied which is used for registration, when the entered value satisfies all the conditions,the new password is reset sucessfully
                     if len(b) < 5 and len(b) > 16:
                         print("Create Password with length between 5 and 16, Try Again")
                         register()
@@ -40,19 +40,18 @@ def login():
                                 l += 1
                             if i.isupper():
                                 u += 1
-                            if (
-                                    i == '@' or i == '$' or i == '_' or i == '%' or i == '!' or i == '#' or i == '*' or i == '&'):
+                            if (i == '@' or i == '$' or i == '_' or i == '%' or i == '!' or i == '#' or i == '*' or i == '&'):
                                 p += 1
                             if (l >= 1 and u >= 1 and p >= 1 and d >= 1 and l + p + u + d == len(b)):
                                 s = True
 
-                    if s:
+                     if s:
                         c = input("Confirm Password: ")
                         while (c != b):
                             print("Password not match, Try Again")
                             c = input("Try Again: ")
 
-                    else:
+                     else:
                         print("Sorry,Try again to login")
                         login()
 
@@ -60,6 +59,6 @@ def login():
                     file.write(X + "," + b + "\n")
                     file.close()
 
-    else:
+     else:
         print("Unregistered user:To login your account, you need to register first")
         register()
