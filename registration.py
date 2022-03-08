847cb5d8a1dae3c05d579edb75f43f17cd391b0d
Priyadharshini <87526837+Priyasivakum@users.noreply.github.com>
2022-03-08 15:10:44 +0530

    Update registration.py

--- registration.py
+++ registration.py
@@ -5,9 +5,13 @@ def register():
     for line in db:
         x = line.split(",")
         d.append(x[0])
+    #if email already exists say to login by entering password
      if a in d:
         print("user name already exists,Try login")
         register()
+     #username format: it should begin with special char but can contain special char and not necessary to have digits and uppercase.
+    #when it satisfies the above username is created and added to the text file named user_detail
+    
      elif a.count('@') != 1 and a.count('.') != 1:
         print("enter username in proper format")
         register()
@@ -17,13 +21,14 @@ def register():
         register()
      else:
         print("Username created")
+ #password format: must include atleast 1 digit,1 uppercase and 1 special char and length should not exceed 15 char
  b = input("Create your password with atleast one capital letter one integer and one special character: ")
     s = False
 
     if len(b) < 5 and len(b) > 16:
         print("Create Password with length between 5 an 16, Try Again")
         register()
-
+#when it passes the first condition checks the remaining  includes number,uppercase & special char,initialized to zero
     if len(b) > 5 and len(b) < 16:
         l, u, p, d = 0, 0, 0, 0
         for i in b:
@@ -37,9 +42,10 @@ def register():
                 p += 1
             if (l >= 1 and u >= 1 and p >= 1 and d >= 1 and l + p + u + d == len(b)):
                 s = True
-
+#when password is entered in correct format,user is registered successfully
     if s:
         c = input("Confirm Password: ")
+        print("user registered successfully")
         while (c != b):
             print("Password not match, Try Again")
             c = input("Try Again: ")  
